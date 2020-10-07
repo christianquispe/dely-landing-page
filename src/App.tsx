cc640eeba19cd4cfb9924c997c8cb3827bd85cd2
christian quispe <christianquispecamasca@gmail.com>
2020-10-06 22:19:51 -0500

    chore: create example component and add scss styles(#2)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.scss";
+const App = () => {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
