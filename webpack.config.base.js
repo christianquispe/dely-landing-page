7a1c93ff1c35ce334a6cc376ac0bcd019a620a44
christian quispe <christianquispecamasca@gmail.com>
2020-10-06 22:19:24 -0500

    chore: add sass loader to webpack(#2)

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -39,6 +39,17 @@ module.exports = {
         ],
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.(png|jpg|gif|mp3|ico)$/,
         use: [
           {
