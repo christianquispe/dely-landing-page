6570b6f93aa0056e2a80ab2cc33df882bf79443d
@erick <erick.hx22@gmail.com>
2020-10-07 21:49:29 -0500

    refactor: rename app to  route app (#11)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import RouteApp from "./router";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<RouteApp />, document.getElementById("root"));
