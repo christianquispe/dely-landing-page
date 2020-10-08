27f0acd6515e626f63b779bd49ef4bf3ff9c5a81
@erick <erick.hx22@gmail.com>
2020-10-07 21:48:21 -0500

    chore: implement pages route with react-router (#11)

---
+++ src/router/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// Here goes Routing
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import RouteComponent from "./Route";
+import { IRoutes, routes } from "./routes";
+
+const RouteApp: React.FC = () => (
+  <Router>
+    <Switch>
+      {routes.map((route: IRoutes) => {
+        return (
+          <RouteComponent
+            exact={route.exact}
+            key={route.id}
+            path={route.path}
+            route={route}
+          />
+        );
+      })}
+    </Switch>
+  </Router>
+);
+
+export default RouteApp;
