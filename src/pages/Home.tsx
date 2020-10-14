b8bacb85dc1ffedb6cecf61826db12f56a55f626
@erick <erick.hx22@gmail.com>
2020-10-14 18:04:39 -0500

    feat: implement banner to home page (#9)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { NavBar } from "../components/Navbar";
+import { NavBar } from "@components/Navbar";
+import { Banner } from "@components/Banner";
 
 export const Home: React.FC = () => {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   );
 };
