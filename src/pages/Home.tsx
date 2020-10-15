870697c900a9a7a1883ddf9945de4e5e7ff420a6
@erick <erick.hx22@gmail.com>
2020-10-14 19:36:32 -0500

    refactor: update banner height (#21)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavBar } from "@components/Navbar";
 import { Banner } from "@components/Banner";
+import { Services } from "@components/Services";
 
 export const Home: React.FC = () => {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Services />
     </div>
   );
 };
