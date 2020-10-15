d6d97c220f915c0837715bff5b054dfcda08e201
christian quispe <christianquispecamasca@gmail.com>
2020-10-15 01:22:21 -0500

    chore: implement about us section(#19)

---
+++ src/components/AboutUs/Section.tsx
@@ -0,0 +1,27 @@
+import { Section } from "@components/Section";
+import { Title } from "@components/Typography";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./styles.scss";
+
+export const AboutUs = () => {
+  return (
+    <section>
+      <Container>
+        <Section>
+          <Row>
+            <Col xs={12} lg={6}>
+                <div className="Box_example"></div>
+            </Col>
+            <Col xs={12} lg={6}>
+                <Title className="AboutUs__title" level={2}>Nosotros</Title>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ut optio voluptatum! Numquam consequuntur ea rerum et! Vitae quo fuga, ducimus aspernatur impedit vero deleniti explicabo dicta consequuntur esse sunt?</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ut optio voluptatum! Numquam consequuntur ea rerum et! Vitae quo fuga, ducimus aspernatur impedit vero deleniti explicabo dicta consequuntur esse sunt?</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ut optio voluptatum! Numquam consequuntur ea rerum et! Vitae quo fuga, ducimus aspernatur impedit vero deleniti explicabo dicta consequuntur esse sunt?</p>
+            </Col>
+          </Row>
+        </Section>
+      </Container>
+    </section>
+  );
+};
