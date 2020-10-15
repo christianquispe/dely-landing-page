666a3c42680786b923249682023df64a10071e01
christian quispe <christianquispecamasca@gmail.com>
2020-10-15 01:23:06 -0500

    chore: add about section to home(#19)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { NavBar } from "@components/Navbar";
+import { AboutUs } from "@components/AboutUs";
 import { Banner } from "@components/Banner";
+import { NavBar } from "@components/Navbar";
 import { Services } from "@components/Services";
+import React from "react";
 
 export const Home: React.FC = () => {
   return (
     <div>
       <NavBar />
       <Banner />
+      <AboutUs />
       <Services />
     </div>
   );
