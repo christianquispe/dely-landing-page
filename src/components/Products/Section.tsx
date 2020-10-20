707187ee400e628ee135fa9eb34885401f41de06
@erick <erick.hx22@gmail.com>
2020-10-19 22:12:09 -0500

    feat: remove unnecessary tabs (#31)

--- src/components/Products/Section.tsx
+++ src/components/Products/Section.tsx
@@ -7,10 +7,14 @@ import "./styles.scss";
 
 export const Products = () => {
   const [key, setKey] = useState("groceries");
+
   const productList = dataProducts.map((product) => {
     return (
       <Col key={product.id} xs={12} md={6} lg={4}>
-        <Card style={{ width: "100%", marginBottom: "20px" }} className="Products__item">
+        <Card
+          style={{ width: "100%", marginBottom: "20px" }}
+          className="Products__item"
+        >
           <Card.Img
             variant="top"
             src="https://cdnx.jumpseller.com/carabela/image/11642227/resize/265/265?1600205590"
@@ -27,58 +31,38 @@ export const Products = () => {
   });
   return (
     <Section className="Products" fluid={true}>
-      <Row>
-        <Col xs={12} md={6}>
-          <Title level={2} className="Products__title">
-            Products
-          </Title>
-        </Col>
-        <Col xs={12}>
-          <Row className="d-flex justify-content-end">
-            <Tabs
-              id="controlled-tab-example"
-              activeKey={key}
-              onSelect={(k) => setKey(k)}
-            >
-              <Tab eventKey="groceries" title="Abarrotes">
-                <div style={{ padding: "20px" }}>
-                  <Row className="d-flex justify-content-between">
-                    {productList}
-                  </Row>
-                </div>
-              </Tab>
-              <Tab eventKey="waters-and-drinks" title="Aguas y bebidas">
-                <div style={{ padding: "20px" }}>
-                  <Row className="d-flex justify-content-between">
-                    {productList}
-                  </Row>
-                </div>
-              </Tab>
-              <Tab eventKey="sausages" title="Embutidos">
-                <div style={{ padding: "20px" }}>
-                  <Row className="d-flex justify-content-between">
-                    {productList}
-                  </Row>
-                </div>
-              </Tab>
-              <Tab eventKey="cleaning" title="Limpieza">
-                <div style={{ padding: "20px" }}>
-                  <Row className="d-flex justify-content-between">
-                    {productList}
-                  </Row>
-                </div>
-              </Tab>
-              <Tab eventKey="healt-hand-beauty" title="Salud y belleza">
-                <div style={{ padding: "20px" }}>
-                  <Row className="d-flex justify-content-between">
-                    {productList}
-                  </Row>
-                </div>
-              </Tab>
-            </Tabs>
-          </Row>
-        </Col>
-      </Row>
+      <div className="Products__heading">
+        <Title level={2} className="Products__title">
+          Products
+        </Title>
+        <Tabs
+          id="controlled-tab-example"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+        >
+          <Tab eventKey="groceries" title="Abarrotes">
+            <div style={{ padding: "20px" }}>
+              <Row className="d-flex justify-content-between">
+                {productList}
+              </Row>
+            </div>
+          </Tab>
+          <Tab eventKey="waters-and-drinks" title="Aguas y bebidas">
+            <div style={{ padding: "20px" }}>
+              <Row className="d-flex justify-content-between">
+                {productList}
+              </Row>
+            </div>
+          </Tab>
+          <Tab eventKey="sausages" title="Embutidos">
+            <div style={{ padding: "20px" }}>
+              <Row className="d-flex justify-content-between">
+                {productList}
+              </Row>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
     </Section>
   );
 };
