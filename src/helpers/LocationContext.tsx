ef9bcadcba1545fcbf1bf652a567c5157658c79b
christian quispe <christianquispecamasca@gmail.com>
2020-10-19 21:55:46 -0500

    chore: change img example for locations(#30)

--- src/helpers/LocationContext.tsx
+++ src/helpers/LocationContext.tsx
@@ -17,13 +17,13 @@ export const LocationContextDefault = [
     name: "Ubication 3",
     address: "Av. Avenida 1 Nro. 1234 Mz. A-1 Lote 00",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX6JXUGtXu0hq4Bhmvvzotp0ZiRVJ1G8u8rA&usqp=CAU",
+      "https://image.freepik.com/vector-gratis/plano-ubicacion-icono-vector-diseno-plano_39704-211.jpg",
   },
   {
     name: "Ubication 4",
     address: "Av. Avenida 1 Nro. 1234 Mz. A-1 Lote 00",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX6JXUGtXu0hq4Bhmvvzotp0ZiRVJ1G8u8rA&usqp=CAU",
+      "https://es.wikineos.com/images/thumb/7/79/Topographic_map_example.png/300px-Topographic_map_example.png",
   },
 ];
 
