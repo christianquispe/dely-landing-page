db490278d305433f6d35d6e57f532af6fd6817b0
christian quispe <christianquispecamasca@gmail.com>
2020-10-20 11:29:43 -0500

    chore: update home page(#10)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,23 @@
 import { AboutUs } from "@components/AboutUs";
 import { Banner } from "@components/Banner";
 import { Brands } from "@components/Brands";
+import { WhatsAppFixed } from "@components/Fixed";
 import { Footer } from "@components/Footer";
 import { Location } from "@components/Location";
 import { NavBar } from "@components/Navbar";
 import { Products } from "@components/Products";
 import { Services } from "@components/Services";
 import { Supports } from "@components/Supports";
-
 import LocationsContext, {
-  LocationContextDefault,
+  LocationContextDefault
 } from "@helpers/LocationContext";
 import React from "react";
+
 
 export const Home: React.FC = () => {
   return (
     <div>
+      <WhatsAppFixed />
       <NavBar />
       <Banner />
       <AboutUs />
