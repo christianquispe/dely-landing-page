81d573e74721df189e7017b251a7683f6e284572
@erick <erick.hx22@gmail.com>
2020-10-21 22:31:32 -0500

    feat: implement constants for env variables (#35)

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const USER_ID = process.env.USER_ID;
+export const TEMPLATE_ID = process.env.TEMPLATE_ID;
+export const SERVICE_ID = process.env.SERVICE_ID;
+export const TO_NAME = process.env.TO_NAME;
+export const TO_EMAIL = process.env.TO_EMAIL;
+export const SUBJECT_MAIL = process.env.SUBJECT_MAIL;
