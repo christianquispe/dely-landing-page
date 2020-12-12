582522f2fcfdba0da62d49daa0cf9693d6edec89
christian quispe <christianquispecamasca@gmail.com>
2020-12-12 17:26:37 -0500

    chore: update aceites data(#50)

--- src/data/aceitesData.ts
+++ src/data/aceitesData.ts
@@ -1,34 +1,17 @@
 import aceiteChico from "@assets/productos/lenysol-aceite-chico.jpg";
 import aceiteGrande from "@assets/productos/lenysol-aceite-grande.jpg";
+import aceite5Litros from "@assets/productos/aceite-5l.jpg";
+import aceite2Litros from "@assets/productos/aceite-2l.jpg";
+import aceite450Ml from "@assets/productos/aceite-450ml.jpg";
 
 export const aceiteData = [
   {
     id: 7,
     companyId: 1,
     brand: "ACEITE VEGETAL",
-    name: "LENYSOL ACEITE VEGETAL x 12 und x 1L",
+    name: "LENYSOL ACEITE VEGETAL 1L",
     description: "PRODUCTO PERUANO",
-    image: aceiteGrande,
-    price: 100,
-    discount: 10,
-  },
-  {
-    id: 8,
-    companyId: 1,
-    brand: "ACEITE VEGETAL",
-    name: "LENYSOL ACEITE x 12und x 450 ml",
     image: aceiteChico,
-    description: "PRODUCTO PERUANO",
-    price: 100,
-    discount: 10,
-  },
-  {
-    id: 9,
-    companyId: 1,
-    brand: "ACEITE VEGETAL",
-    name: "LENYSOL ACEITE x 4und x 5L",
-    image: aceiteGrande,
-    description: "PRODUCTO PERUANO",
     price: 100,
     discount: 10,
   },
@@ -36,18 +19,40 @@ export const aceiteData = [
     id: 10,
     companyId: 1,
     brand: "ACEITE VEGETAL",
-    name: "LENYSOL ACEITE x 12und x 900ml",
+    name: "LENYSOL ACEITE 900ml",
     image: aceiteGrande,
     description: "PRODUCTO PERUANO",
     price: 100,
     discount: 10,
   },
   {
+    id: 8,
+    companyId: 1,
+    brand: "ACEITE VEGETAL",
+    name: "LENYSOL ACEITE 450 ml",
+    image: aceite450Ml,
+    description: "PRODUCTO PERUANO",
+    price: 100,
+    discount: 10,
+  },
+
+  {
     id: 11,
     companyId: 1,
     brand: "ACEITE VEGETAL",
-    name: "LENYSOL ACEITE x 6und x 2L",
-    image: aceiteGrande,
+    name: "LENYSOL ACEITE 2L",
+    image: aceite2Litros,
+    description: "PRODUCTO PERUANO",
+    price: 100,
+    discount: 10,
+  },
+
+  {
+    id: 9,
+    companyId: 1,
+    brand: "ACEITE VEGETAL",
+    name: "LENYSOL ACEITE 5L",
+    image: aceite5Litros,
     description: "PRODUCTO PERUANO",
     price: 100,
     discount: 10,
