46ba0f9a0a09b5f7a6559f41dcad749cf9ac99b4
christian quispe <christianquispecamasca@gmail.com>
2020-12-14 10:26:55 -0500

    chore: repair layout responsive on services(#52)

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -2,7 +2,8 @@ import { Section } from "@components/Section";
 import { Title } from "@components/Typography";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import image from "../../assets/servicios/1.png";
+import imageVentasInst from "../../assets/servicios/ventas_inst.jpg";
+import imageVentasCorp from "../../assets/servicios/ventas.jpg";
 import "./Services.scss";
 
 export const Services = () => {
@@ -10,65 +11,66 @@ export const Services = () => {
     <Section
       className="Services section-common-home--white"
       fluid={true}
-      id="products-and-services"
+      id="services"
     >
-      <Container className="section-common-home">
-        <Title level={2} className="Services__title text-center">
-          Servicios
-        </Title>
-        <div className="services-content-layout">
-          <div>
-            <div className="Services__content">
-              <Title level={2} className="text-center">
-                Ventas Institucionales
-              </Title>
-              <p>
-                Estamos dispuestos a brindar un servicio especializado y precios
-                justos, además contamos con todos los certificados
-                correspondientes y el Registro Nacional de Proveedores (RNP)
-                vigente.
-              </p>
-            </div>
-            <div className="d-flex justify-content-center">
-              <img src={image} alt="" className="Services__img" />
-            </div>
+      <Title level={2} className="Services__title text-center">
+        Servicios
+      </Title>
+      <div className="services-content-layout">
+        <div className="Service">
+          <div className="Services__content">
+            <Title level={3} className="Services__subtitle text-center">
+              Ventas Institucionales
+            </Title>
+            <p>
+              Estamos dispuestos a brindar un servicio especializado y precios
+              justos, además contamos con todos los certificados
+              correspondientes y el Registro Nacional de Proveedores (RNP)
+              vigente.
+            </p>
           </div>
-          <div className="divider-horizontal"></div>
-          <Col xs={12} md={6}>
-            <div>
-              <Title level={2} className="text-center">
-                Ventas Corporativa
-              </Title>
-              <p>
-                Contamos con personal efectivo dispuesto a ofrecer productos de
-                calidad y con las mejores ofertas del mercado, para distintos
-                fines tales como: Donaciones, Productos para la realización de
-                canastas, entre otros.
-              </p>
-            </div>
-          </Col>
+          <div className="d-flex justify-content-center">
+            <img src={imageVentasInst} alt="" className="Services__img" />
+          </div>
         </div>
-        <Row className="Services__list">
+        <div className="divider-horizontal"></div>
+        <div className="Service">
           <div>
-            <div className="Services__item">
-              <div className="Services__item__img"></div>
-              <span>Gobieno Regional</span>
-            </div>
+            <Title level={3} className="Services__subtitle text-center">
+              Ventas Corporativa
+            </Title>
+            <p>
+              Contamos con personal efectivo dispuesto a ofrecer productos de
+              calidad y con las mejores ofertas del mercado, para distintos
+              fines tales como: Donaciones, Productos para la realización de
+              canastas, entre otros.
+            </p>
           </div>
+          <div className="d-flex justify-content-center">
+            <img src={imageVentasCorp} alt="" className="Services__img" />
+          </div>
+        </div>
+      </div>
+      <Row className="Services__list">
+        <div>
           <div className="Services__item">
             <div className="Services__item__img"></div>
-            <span>Qaliwarma</span>
+            <span>Gobieno Regional</span>
           </div>
-          <div className="Services__item">
-            <div className="Services__item__img"></div>
-            <span>Municipalidades</span>
-          </div>
-          <div className="Services__item">
-            <div className="Services__item__img"></div>
-            <span>Corporativos</span>
-          </div>
-        </Row>
-      </Container>
+        </div>
+        <div className="Services__item">
+          <div className="Services__item__img"></div>
+          <span>Qaliwarma</span>
+        </div>
+        <div className="Services__item">
+          <div className="Services__item__img"></div>
+          <span>Municipalidades</span>
+        </div>
+        <div className="Services__item">
+          <div className="Services__item__img"></div>
+          <span>Corporativos</span>
+        </div>
+      </Row>
     </Section>
   );
 };
